73f810c1f2bd86228662744aebf5fcd3e42a4722
HACK086 <zhalil.p.1303@gmail.com>
2023-04-30 17:41:55 +0300

    finish

--- Wordpress/main/models.py
+++ Wordpress/main/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
+# Импортирование модуля models из библиотеки Django
+
 class Section(models.Model):
+    # Определение модели данных для раздела
     title = models.CharField(max_length=100)
 
     def __str__(self):
+        # Возвращение названия раздела в качестве строкового представления объекта
         return self.title
 
 class Subsection(models.Model):
+    # Определение модели данных для подраздела
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
 
     def __str__(self):
+        # Возвращение названия подраздела в качестве строкового представления объекта
         return self.title
--- Wordpress/main/views.py
+++ Wordpress/main/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
-from .models import Section, Subsection
+# Импортирование необходимых модулей и моделей из приложения
 from django.shortcuts import render, get_object_or_404
+from .models import Section, Subsection
+
+
 
 def section_list(request):
+    # Получение списка всех объектов разделов
     sections = Section.objects.all()
+    # Отображение списка разделов в шаблоне 'main/section_list.html' с использованием функции render()
     return render(request, 'main/section_list.html', {'sections': sections})
 
 def subsection_list(request, section_id):
+    # Получение списка всех подразделов, связанных с определенным разделом
     subsections = Subsection.objects.filter(section=section_id)
+    # Отображение списка подразделов в шаблоне 'main/subsection_list.html' с использованием функции render()
     return render(request, 'main/subsection_list.html', {'subsections': subsections})
 
 def subsection_detail(request, subsection_id):
+    # Получение подробной информации о конкретном подразделе
     subsection = get_object_or_404(Subsection, id=subsection_id)
+    # Отображение информации о подразделе в шаблоне 'main/subsection_detail.html' с использованием функции render()
     return render(request, 'main/subsection_detail.html', {'subsection': subsection})
